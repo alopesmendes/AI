e81bd9d92c8b85c7d042e22eed4ebf9db838ca46
alopesmendes <ailtonlopesmendes9@gmail.com>
2021-03-08 12:38:35 +0100

    avro

--- CovidFaker/src/main/scala/kafka/ProducerValue.scala
+++ CovidFaker/src/main/scala/kafka/ProducerValue.scala
@@ -2,10 +2,17 @@ package kafka
 
 import person.Vaccine.SideEffects
 
-/***
+/** *
  * The class that will represent the values that will be parse and send
- * @param subject the String of the subject.
- * @param obj the String of the object.
+ *
+ * @param subject    the String of the subject.
+ * @param obj        the String of the object.
  * @param sideEffect the SideEffects of a vaccine.
  */
-case class ProducerValue(val subject : String, val obj : String, val sideEffect: SideEffects)
+case class ProducerValue(
+                        val id: String,
+                        val fName: String,
+                        val lName: String,
+                        val vaccinationDate: String,
+                        val vaccine: String,
+                        val sideEffect: SideEffects)
